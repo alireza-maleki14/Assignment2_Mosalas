3aa730dd05b3354c1d75302a33e2635af02e2874
alireza-maleki14 <148278866+alireza-maleki14@users.noreply.github.com>
2023-10-18 16:57:18 +0330

    Add files via upload

---
+++ Assignment2_Mosalas.py
@@ -0,0 +1,10 @@
+print("Azlae Mosalas ra Vared Konid:")
+a=int(input("Enter A:"))
+b=int(input("Enter B:"))
+c=int(input("Enter C:"))
+
+
+if a+b>c and a+c>b and c+b>a:
+    print("Mosalas Ast")
+else:
+    print("Mosalas Nist")
